a796a1fb0e9d213320b8c01d90b209bf75d80f5b
amatgil <amatgilvinyes@gmail.com>
2024-11-15 01:00:26 +0100

    Only closing parens are missing for the shunting yard algorithm

--- src/computacio.rs
+++ src/computacio.rs
@@ -1,3 +1,5 @@
+use core::mem;
+
 use crate::*;
 
 pub fn compute_tokens(toks: &[Option<Token>]) -> Result<TextArea, ComputationError> {
@@ -51,31 +53,39 @@ fn to_postfix(
     use PseudoOp as POp;
     use PseudoToken as PT;
 
-    let mut op_stack: [Option<POp>; MAX_TOKENS] = [None; MAX_TOKENS];
-    let mut op_idx = 0; // Where we are in the operator stack
-
-    let mut output_stack: [Option<PT>; MAX_TOKENS] = [None; MAX_TOKENS];
-    let mut output_idx = 0; // Where we are in the output stack
+    let mut op_stack: Stack<POp> = Stack::default();
+    let mut output_stack: Stack<PT> = Stack::default();
 
     let mut current_number: Option<Enter> = None; // Tokens go digit by digit, this is where the partial number is stored
     let mut token_idx = 0;
     while token_idx < MAX_TOKENS {
         let Some(token) = input[token_idx] else { break };
+
+        // If we were parsing a number but it's over, push to output stack
+        if current_number.is_some() && !matches!(token, Token::Digit(_)) {
+            output_stack.push(PseudoToken::Number(current_number.unwrap()));
+            current_number = None;
+        }
+
         match token {
             Token::Digit(d) => match current_number {
                 None => current_number = Some(d as Enter),
                 Some(n) => current_number = Some(n * 10 + d as Enter),
             },
             Token::Op(curr_op) => {
-                // SAFETY: shortcircuiting means we've always seen one operator when we index into it
-                if op_idx == 0 || (PseudoOp::Op(curr_op) > op_stack[op_idx].unwrap()) {
-                    push_to_op_stack(&mut op_stack, &mut op_idx, POp::Op(curr_op));
+                // SAFETY: shortcircuiting means !is_empty() when we unwrap the top() call
+                if op_stack.is_empty() || (PseudoOp::Op(curr_op) > *op_stack.top().unwrap()) {
+                    op_stack.push(POp::Op(curr_op));
                 } else {
-                    // TODO
+                    // op_stack isn't empty from the previous condition (de Morgan my beloved)
+                    // TODO: Verify this is what the algorithm says
+                    let lower_op = op_stack.pop().unwrap();
+                    output_stack.push(PT::Op(lower_op));
+                    op_stack.push(POp::Op(curr_op));
                 }
             }
             Token::Paren(Paren::Open) => {
-                push_to_op_stack(&mut op_stack, &mut op_idx, PseudoOp::Paren(Paren::Open))
+                op_stack.push(PseudoOp::Paren(Paren::Open));
             }
             Token::Paren(Paren::Close) => {
                 let end_range = op_idx; // Non-inclusive
@@ -93,9 +103,8 @@ fn to_postfix(
                 if token_idx + 1 < MAX_TOKENS {
                     match input[token_idx + 1] {
                         Some(Token::Dist(d)) => {
-                            push_to_op_stack(&mut op_stack, &mut op_idx, PseudoOp::FuncioR(v, d));
+                            op_stack.push(PseudoOp::FuncioR(v, d));
                             token_idx += 1; // We took 2 instead of 1
-                            op_idx += 1;
                         }
                         _ => return Err(ComputationError::RVariantNotFollowedByDistFn),
                     }
@@ -104,23 +113,13 @@ fn to_postfix(
         }
 
         token_idx += 1;
-        if current_number.is_some() && !matches!(token, Token::Digit(_)) {
-            push_to_output_stack(
-                &mut output_stack,
-                &mut output_idx,
-                PseudoToken::Number(current_number.unwrap()),
-            );
-            current_number = None;
-        }
     }
 
-    while op_idx > 0 {
-        op_idx -= 1;
-        let op = op_stack[op_idx].unwrap();
-        push_to_output_stack(&mut output_stack, &mut output_idx, PseudoToken::Op(op));
+    while let Some(op) = op_stack.pop() {
+        output_stack.push(PT::Op(op));
     }
 
-    Ok(output_stack)
+    Ok(output_stack.as_elements())
 }
 
 fn push_to_op_stack(
@@ -151,10 +150,10 @@ fn basic_to_postfix() {
         Some(Token::Digit(7)),
         Some(Token::Digit(7)),
         Some(Token::Digit(7)),
-        None,
-        None,
-        None,
-        None,
+        Some(Token::Op(Operacio::Add)),
+        Some(Token::Digit(6)),
+        Some(Token::Digit(5)),
+        Some(Token::Digit(4)),
         None,
         None,
         None,
@@ -165,7 +164,7 @@ fn basic_to_postfix() {
         None,
         None,
     ];
-    let mut output = [
+    let output = [
         Some(PseudoToken::Number(123)),
         Some(PseudoToken::Number(777)),
         Some(PseudoToken::Op(PseudoOp::Op(Operacio::Add))),
@@ -190,6 +189,7 @@ fn basic_to_postfix() {
     let res = to_postfix(&input).unwrap_or_else(|e| panic!("Failed {e:?}"));
     for i in 0..MAX_TOKENS {
         if res[i] != output[i] {
+            dbg!(res, output);
             panic!("Mismatch at {i}: {:?}, {:?}", res[i], output[i]);
         }
     }
@@ -204,4 +204,68 @@ impl PartialOrd for PseudoOp {
     }
 }
 
-struct Stack {}
+struct Stack<T> {
+    /// Held elements
+    elements: [Option<T>; MAX_TOKENS],
+    /// Possible pointer to the last inserted element
+    idx: Option<usize>,
+}
+
+impl<T> Stack<T> {
+    /// Returns Err when amount of elements exceeds MAX_TOKENS
+    fn push(&mut self, elem: T) -> Result<(), ()> {
+        match self.idx {
+            None => {
+                self.elements[0] = Some(elem);
+                self.idx = Some(0);
+                Ok(())
+            }
+            Some(i) => {
+                if (i + 1) >= MAX_TOKENS {
+                    return Err(());
+                }
+                self.elements[i + 1] = Some(elem);
+                self.idx = Some(i + 1);
+                Ok(())
+            }
+        }
+    }
+    fn pop(&mut self) -> Option<T> {
+        match self.idx {
+            None => None,
+            Some(i) => {
+                let mut x = None;
+                mem::swap(&mut x, &mut self.elements[i]);
+                if i == 0 {
+                    self.idx = None
+                } else {
+                    self.idx = Some(i - 1)
+                }
+                // SAFETY: Invariant holds, this was pointed to by `i` so it must be valid
+                Some(x.unwrap())
+            }
+        }
+    }
+    fn is_empty(&self) -> bool {
+        self.idx.is_none()
+    }
+    fn top(&self) -> Option<&T> {
+        match self.idx {
+            None => None,
+            // SAFETY: pointed to by i, must be Some
+            Some(i) => Some(self.elements[i].as_ref().unwrap()),
+        }
+    }
+    fn as_elements(self) -> [Option<T>; MAX_TOKENS] {
+        self.elements
+    }
+}
+
+impl<T> Default for Stack<T> {
+    fn default() -> Self {
+        Self {
+            elements: [const { None }; MAX_TOKENS],
+            idx: None,
+        }
+    }
+}
